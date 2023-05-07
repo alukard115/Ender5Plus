d3485c224fa08c21edf26a0f497eb5a9679a6d80
Alexander Santos <alexxs463@gmail.com>
2023-05-07 15:33:27 -0600

    cambios ender 5 plus

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1755,7 +1755,7 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 340
+#define X_BED_SIZE 300
 #define Y_BED_SIZE 340
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
